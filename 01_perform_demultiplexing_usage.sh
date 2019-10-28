622e2631a5dcf6f223bc17bdf8a7f43c992a4919
Gaurav Jain <gauravj49@gmail.com>
2019-10-28 13:06:39 +0100

    Modified to work with different base strings

--- 01_perform_demultiplexing_usage.sh
+++ 01_perform_demultiplexing_usage.sh
@@ -6,11 +6,21 @@ cd /home/rad/users/gaurav/projects/misc
 
 # 1) Christine Klement
 # 1.1) For the first run for ATACseq_MUC001 sampels
+
+# Reverse compliment
+ipython
+old_chars = "ACGT"
+rep_chars = "TGCA"
+repreg    = str.maketrans(old_chars,rep_chars)
+dnastr    = ['TCGCCTTA','CTAGTACG','TTCTGCCT','GCTCAGGA','AGGAGTCC','CATGCCTA','GTAGAGAG','CCTCTCTG','AGCGTAGC','CAGCCTCG','TGCCTCTT','TCCTCTAC','ATCACGAC','ACAGTGGT','CAGATCCA','TGTGACCA','GAAACCCA','CCCAACCT']
+revcomp   = [d.translate(repreg)[::-1] for d in dnastr]
+
 # NOTE: The I7 indexes were reverse complimented before running the demultiplexing. The original_samplesheet.csv has original indexes.
-# sudo bash scripts/perform_demultiplexing.sh /media/nas/raw/TUM_Nextseq/190808_NB501802_0188_AHKMYFBGXB /media/nas/fastq/Studies/AGRad_ATACseq_MUC001
-inputDir="/home/rad/users/gaurav/projects/misc/input/190808_NB501802_0188_AHKMYFBGXB"
-outputDir="/home/rad/users/gaurav/projects/misc/output/AGRad_ATACseq_MUC001"
-sudo /usr/local/bin/bcl2fastq -R  ${inputDir} -p 12 --output-dir ${outputDir} --no-lane-splitting --use-bases-mask Y76,I8,n8
+sudo bash scripts/perform_demultiplexing.sh /media/nas/raw/TUM_Nextseq/190808_NB501802_0188_AHKMYFBGXB /media/nas/fastq/Studies/AGRad_ATACseq_MUC001_2 24 /media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001/demultiplexing "Y76,I8,n8"
+
+# inputDir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001/demultiplexing/input/190808_NB501802_0188_AHKMYFBGXB"
+# outputDir="/media/rad/SSD1/atac_temp/christine/AGRad_ATACseq_MUC001/demultiplexing/output/AGRad_ATACseq_MUC001"
+# sudo /usr/local/bin/bcl2fastq -R  ${inputDir} -p 12 --output-dir ${outputDir} --no-lane-splitting --use-bases-mask Y76,I8,n8
 
 # Run fastqc
 fastqcDir="${outputDir}/fastqc"; mkdir -p ${fastqcDir}
@@ -25,3 +35,9 @@ fastqc ${f} -o ${fastqcDir} -q ;\
 # Run multiqc
 multiqcDir="${outputDir}/multiqc"; mkdir -p ${multiqcDir}
 multiqc -o ${multiqcDir} -n AGRad_ATACseq_MUC001 ${outputDir}
+
+# 1.2) Test for ChIPseq antibodies
+# sudo bash scripts/perform_demultiplexing.sh /media/nas/raw/TUM_Nextseq/191023_NB501802_0215_AHC27TBGXC /media/nas/fastq/Studies/AGRad_ChiPseq_AntibodyTest_1 24 /media/rad/HDD1/temp_chip/AGRad_ChiPseq_AntibodyTest_1/demultiplexing "Y61,I6,Y101"
+inputDir="/media/rad/HDD1/temp_chip/AGRad_ChiPseq_AntibodyTest_1/demultiplexing/input/191023_NB501802_0215_AHC27TBGXC"
+outputDir="/media/rad/HDD1/temp_chip/AGRad_ChiPseq_AntibodyTest_1/demultiplexing/output/AGRad_ChiPseq_AntibodyTest_1"
+sudo /usr/local/bin/bcl2fastq -R  ${inputDir} -p 12 --output-dir ${outputDir} --no-lane-splitting --use-bases-mask Y61,I6,Y101
--- scripts/perform_demultiplexing.sh
+++ scripts/perform_demultiplexing.sh
@@ -7,6 +7,7 @@ rInDir=${1}                     # remote input  directory: "/media/nas/raw/TUM_N
 rOpDir=${2}                     # remote output directory: "/media/nas/fastq/Studies/AGRad_ATACseq_MUC001"
 numThreads=${3:-"12"}           # -p [ --processing-threads ] number of threads used for processing demultiplexed data
 jobdir=${4:-"/home/rad/users/gaurav/projects/misc"}
+baseMask=${5:-""}               # Y76,I8,n8
 
 # Get the system variables
 bcltofastq=${5:-"$(which bcl2fastq)"}   # /usr/local/bin/bcl2fastq`
@@ -23,17 +24,18 @@ lInDir="${jobdir}/input/$(basename ${rInDir})"  # local  input  directory: "/hom
 lOpDir="${jobdir}/output/$(basename ${rOpDir})" # local  output directory: "/home/rad/users/gaurav/projects/misc/output/ATACseq_MUC001"
 echo -e "\t- Local input  directory: ${lInDir}"
 echo -e "\t- Local output directory: ${lOpDir}"
-mkdir -p ${rOpDir} ${lOpDir}
+sudo mkdir -p ${rInDir} ${rOpDir} ${lInDir} ${lOpDir}
 
 # Copy the data from the remote directory to the local input directory
 echo -e "\n- Copying the data from the remote directory to the local input directory..."
-echo -r "- rsync -arvP ${rInDir}/* ${lInDir}"
+echo -e "- rsync -arvP ${rInDir}/* ${lInDir}"
 rsync -arvP ${rInDir}/* ${lInDir}
 
 # Run the demultiplxing
 echo -e "\n- Running demultiplexing using bcl2fastq"
-echo " eval ${bcl2fastq} -R ${lInDir} -p ${numThreads} --output-dir ${lOpDir} --no-lane-splitting"
-eval ${bcltofastq} -R ${lInDir} -p ${numThreads} --output-dir ${lOpDir} --no-lane-splitting
+echo " eval ${bcl2fastq} -R ${lInDir} -p ${numThreads} --output-dir ${lOpDir} --no-lane-splitting --use-bases-mask ${baseMask}"
+# eval ${bcltofastq} -R ${lInDir} -p ${numThreads} --output-dir ${lOpDir} --no-lane-splitting
+eval ${bcltofastq} -R ${lInDir} -p ${numThreads} --output-dir ${lOpDir} --no-lane-splitting --use-bases-mask ${baseMask}
 
 # Copy the data back to the remote output directory from the local output directory
 echo -e "\n- Copying the data back to the remote output directory from the local output directory..."
